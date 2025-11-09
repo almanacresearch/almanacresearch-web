16002cd8801e74270262fe282c863a730409b4f5
yash-almanacresearch <yash@almanacresearch.com>
2025-11-09 15:57:09 +0530

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://almanacresearch.com"
-      : undefined,
+
+  // Redirect non-www requests to the www domain
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "almanacresearch.com",
+          },
+        ],
+        destination: "https://www.almanacresearch.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
